1ce8609bb0f5adbdb5854d4cce152f9e66ed590d
Youssef Tlidi <youssef.tlidi@hotmail.com>
2023-02-16 18:20:27 -0800

    Done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a char: %1u byte(s)\n", (int)sizeof(a));
 printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %1u byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(d));
