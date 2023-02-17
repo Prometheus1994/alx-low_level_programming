9e364976bc02edabfef85099c3eaa16af1609514
Youssef Tlidi <youssef.tlidi@hotmail.com>
2023-02-16 18:10:11 -0800

    Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,7 @@
  * Return: 1
  */
 int main(void)
-{fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 	return (1);
 }
