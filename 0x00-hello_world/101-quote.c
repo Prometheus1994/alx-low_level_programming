d7bc506d690d161472be777100fd2ab3d0eeae47
Youssef Tlidi <youssef.tlidi@hotmail.com>
2023-02-16 17:46:54 -0800

    Done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A C program printing a line to standard error
+ * return: 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}
