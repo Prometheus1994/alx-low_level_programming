014c8cd6586d0053ba0b4571737d83c59ec18853
Youssef Tlidi <youssef.tlidi@hotmail.com>
2023-02-21 17:35:31 -0800

    Done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * main - Test function for positive or negative "Done"
+  * main - Test function for positive or negative
   * Return: 0
   */
 
@@ -14,4 +14,3 @@ int main(void)
 
 return (0);
 }
-
