3d5604eed9929688365c62b1f21a678b2e96d1eb
Youssef Tlidi <youssef.tlidi@hotmail.com>
2023-05-18 13:40:31 -0700

    Done

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,10 +3,9 @@
 #include <string.h>
 
 /**
- * main - Generates and prints passwords for the crackme5 executable.
+ * main - Generates and prints passwords for the crackme5 executable. "Done"
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
- *
  * Return: Always 0.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
