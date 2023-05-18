dacf5545c1c9696042fe2436a8ae23dd121b8f8c
Youssef Tlidi <youssef.tlidi@hotmail.com>
2023-05-18 13:20:20 -0700

    Done

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a doubly linked list "Done"
+ * @head: pointer to the list to free
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *tmp;
+while (head != NULL)
+{
+tmp = head;
+head = head->next;
+free(tmp);
+}
+}
